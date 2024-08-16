1a35f023ecfa0c4f4a2ca9d7f7a1cb514ca53d88
Cromes85 <idv230685@gmail.com>
2024-08-16 12:02:55 +0200

    - url, '_blank'

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function openLink(url) {
 
 function openInPrivate(url) {
   // Utilisation de Microsoft Edge pour ouvrir une nouvelle fenêtre InPrivate
-  let edgePath = 'microsoft-edge:';
-  window.open(edgePath + '--inprivate ' + url, '_blank');
+  let edgePath = "microsoft-edge:";
+  // window.open(edgePath + '--inprivate ' + url, '_blank');
+  window.open(edgePath + "--inprivate ");
 }
