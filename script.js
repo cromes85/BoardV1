dff53b508571323a2f224e90638171d9fd735a0d
Cromes85 <idv230685@gmail.com>
2024-08-16 11:20:54 +0200

    change js et html

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function openLink(url) {
   window.open(url, "_blank");
 }
+
+function openInPrivate(url) {
+  // Utilisation de Microsoft Edge pour ouvrir une nouvelle fenêtre InPrivate
+  let edgePath = 'microsoft-edge:';
+  window.open(edgePath + '--inprivate ' + url, '_blank');
+}
